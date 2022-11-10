bb0dee5ff908b3ac2d59d3fc88652e6f360c047d
Reshinth Adithyan <36307201+reshinthadithyan@users.noreply.github.com>
2022-11-10 21:38:26 +0530

    Bug fix

--- model/rejection_sampling.py
+++ model/rejection_sampling.py
@@ -77,8 +77,8 @@ if __name__ == "__main__":
 
     parser.add_argument("--code_gen_model_name_or_path", type=str, default="Salesforce/codegen-350M-mono")
     parser.add_argument("--generation_tokenizer_name_or_path", type=str, default="Salesforce/codegen-350M-mono")
-    parser.add_argument("--ce_name_or_path", type=str, default="gpt2")
-    parser.add_argument("--ce_tokenizer_name_or_path", type=str, default="gpt2")
+    parser.add_argument("--ce_name_or_path", type=str, default="CarperAI/carptriever-1")
+    parser.add_argument("--ce_tokenizer_name_or_path", type=str, default="CarperAI/carptriever-1")
 
     args = parser.parse_args()
 
